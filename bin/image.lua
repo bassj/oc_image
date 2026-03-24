9a13c005761c3fef2be8a0b6746c6363a1c59ead
Jack Bass <jbass2000@gmail.com>
2026-03-24 17:22:30 -0400

    Check if dir exists before creating it

--- bin/image.lua
+++ bin/image.lua
@@ -1,25 +1,27 @@
+local filesystem = require("filesystem")
 local shell = require("shell")
 
 local repo = "https://raw.githubusercontent.com/bassj/oc_image"
 local branch = "main"
 
+
 local function download_file(path)
-    local v = tostring(math.random(1, 10000000000))
+    local v = string.format("%d", math.random(1, 10000000000))
     local cmd = string.format('wget -f %s/refs/heads/%s%s?v=%s %s', repo, branch, path, v, path)
     shell.execute(cmd)
 end
 
 for _, dir in ipairs({
-    "/lib/bassj",
+    "/lib/bassj/",
 }) do
-    shell.execute("mkdir " .. dir)
+    if not filesystem.isDirectory(dir) then
+        shell.execute("mkdir " .. dir)
+    end
 end
 
 for _, file in ipairs({
     "/bin/image.lua",
     "/bin/component.lua",
-    "/bin/bee.lua",
-    "/bin/ae.lua",
 
     "/lib/json.lua",
     "/lib/bassj/text.lua",
